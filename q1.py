8ef1987b0c514ea81b63ff945488159b647cdecd
bs21b038 <bs21b038@smail.iitm.ac.in>
2025-03-07 16:30:10 +0530

    in q1.py downloaded data into pkl file and in wandb.py ploted the sample data

--- q1.py
+++ q1.py
@@ -1,11 +1,15 @@
 #used env_tf to run the scripts >> requirements.txt
+
+#download the dataset
+
+import pickle
 import numpy as np
 import pandas as pd
 from keras.datasets import fashion_mnist
 
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-print('x_train shape:', x_train.shape)
-print('y_train shape:', y_train.shape)
-print('x_test shape:', x_test.shape)
-print('y_test shape:', y_test.shape)
+with open("fashion_mnist.pkl", "wb") as f:
+    pickle.dump((train_images, train_labels, test_images, test_labels), f)
+
+print('Dataset saved as fashion_mnist.pkl')
