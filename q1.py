3bae850754a76c25c235f1023361fdfc33808820
bs21b038 <bs21b038@smail.iitm.ac.in>
2025-03-07 14:29:54 +0530

    imported libraries

---
+++ q1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from keras.datasets import fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+print('x_train shape:', x_train.shape)
+print('y_train shape:', y_train.shape)
+print('x_test shape:', x_test.shape)
+print('y_test shape:', y_test.shape)
